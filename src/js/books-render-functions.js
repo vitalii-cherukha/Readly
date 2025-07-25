7f8300e4df70ad0c12ac177f03b0023bbdaafd3d
vitalii <vitalii.cherukha@gmail.com>
2025-07-25 10:55:39 +0300

    fix html/css/js

---
+++ src/js/books-render-functions.js
@@ -0,0 +1,42 @@
+const refs = {
+  galleryList: document.querySelector('.books-gallery-list'),
+  categoryList: document.querySelector('.books-nav-category'),
+};
+
+export const createGallery = books => {
+  const galleryCardTemplate = books
+    .map(
+      book =>
+        `<li class="books-gallery-card">
+          <img
+            class="books-gallery-img"
+            src="${book.previewImg}"
+            alt="{book.alt}"
+            width="227"
+            height="323"
+          />
+          <div class="books-gallery-card-info">
+            <h3 class="books-gallery-card-title">Daisy Jones & The Six</h3>
+            <p class="books-gallery-card-author">Taylor Jenkins Reid</p>
+            <p class="books-gallery-card-price">$14</p>
+          </div>
+          <button class="books-gallery-card-btn" type="button">
+            Learn More
+          </button>
+        </li>`
+    )
+    .join('');
+  refs.galleryList.innerHTML = galleryCardTemplate;
+};
+
+export const createCategory = list => {
+  const categoryItemTemplate = list
+    .map(
+      item =>
+        `<li class="books-nav-category-item">
+          <a href="#">${item.category}</a>
+        </li>`
+    )
+    .join('');
+  refs.categoryList.innerHTML = categoryItemTemplate;
+};
