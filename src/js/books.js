dced4a3a84dd37a47a28124054ac6fd171304c86
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-07-26 15:01:24 +0300

    books limit temp

--- src/js/books.js
+++ src/js/books.js
@@ -6,7 +6,7 @@ const renderTopBooks = async () => {
   try {
     const categories = await getTopBooks();
     const allBooks = categories.flatMap(category => category.books);
-    createGallery(allBooks);
+    createGallery(allBooks.slice(15 - 23));
   } catch (error) {
     console.log(error);
   }
