d4bea3b56ee9a25ba69a352d9ad73d05053b5597
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-07-26 19:42:10 +0300

    css-books-tab-desk

--- src/js/books-render-functions.js
+++ src/js/books-render-functions.js
@@ -2,30 +2,32 @@ import { refs } from './refs';
 
 export const createGallery = books => {
   const galleryCardTemplate = books
-    .map(
-      book =>
-        `<li class="books-gallery-card">
-          <img
-            class="books-gallery-img"
-            src="${book.book_image}"
-            alt="${book.description}"
-            width="227"
-            height="323"
-          />
+    .map(book => {
+      const lowTitle = book.title.toLowerCase();
+      const roundPrice = Math.round(Number(book.price));
+
+      return `<li class="books-gallery-card">
+          <div class="books-gallery-img-wrapper">
+            <img
+              class="books-gallery-img"
+              src="${book.book_image}"
+              alt="${book.description}"
+              width="227"
+              height="323"
+            />
+          </div>
           <div class="books-gallery-card-info">
-            <div class="books-galler-card-text">
-              <h3 class="books-gallery-card-title">${book.title.toLowerCase()}</h3>
+            <div class="books-gallery-card-text">
+              <h3 class="books-gallery-card-title">${lowTitle}</h3>
               <p class="books-gallery-card-author">${book.author}</p>
             </div>
-            <p class="books-gallery-card-price">$${Math.round(
-              Number(book.price)
-            )}</p>
+            <p class="books-gallery-card-price">$${roundPrice}</p>
           </div>
           <button class="books-gallery-card-btn" type="button">
             Learn More
           </button>
-        </li>`
-    )
+        </li>`;
+    })
     .join('');
   refs.galleryList.innerHTML = galleryCardTemplate;
 };
