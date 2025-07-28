71e68af930f4284ed63f64288d74d3923fe1c91e
Jackkeetch <eugene.polcanov@gmail.com>
2025-07-28 14:36:10 +0300

    update location and footer.html and .css, added .js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,1 +1,28 @@
-import { refs } from './refs';
+document.addEventListener('footerEventListener', function () {
+  const signupFormBtn = document.getElementById('signupForm');
+  const signUpCloseBtn = document.getElementById('signUpCloseBtn');
+  const footerModal = document.getElementById('footerModal');
+  const emailInput = document.getElementById('footerSubsInput');
+
+  signupFormBtn.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const email = emailInput.value.trim();
+    if (!email) {
+      alert('Пожалуйста, введите email!');
+      return;
+    }
+
+    footerModal.style.display = 'block';
+  });
+
+  signUpCloseBtn.addEventListener('click', function () {
+    footerModal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+      footerModal.style.display = 'none';
+    }
+  });
+});
