dccee43deb2eed7d246affe8e169d5b32539bc26
vitalii <vitalii.cherukha@gmail.com>
2025-07-31 11:10:20 +0300

    min fix

--- src/js/books.js
+++ src/js/books.js
@@ -167,7 +167,9 @@ const onCategoryDropdownClick = async e => {
       const searchValue = e.target.textContent.trim();
       const searchValueSlice = searchValue.slice(0, 20);
       if (searchValueSlice.length < 20) {
-        refs.dropdownToggle.innerHTML = searchValueSlice;
+        refs.dropdownToggle.innerHTML = `${searchValueSlice}<svg class="books-dropdown-arrow" width="24" height="24">
+            <use href="/img/sprite.svg#icon-chevron-down"></use>
+          </svg>`;
       } else {
         refs.dropdownToggle.innerHTML = `${searchValueSlice}..<svg class="books-dropdown-arrow" width="24" height="24">
             <use href="/img/sprite.svg#icon-chevron-down"></use>
